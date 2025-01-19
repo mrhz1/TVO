c5123917b0123ffa3507bb7c340531aa1664302e
Seyed Mohammad Hassanzadeh Amin <smhassanzade@gmail.com>
2025-01-19 16:59:05 -0500

    Showing error and loading messages; fixing wrong error translation ;

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -52,7 +52,6 @@ function App() {
    * If don't have access to location, checking the localStorage to get last searched city name
    */
   useEffect(() => {
-    setError(null);
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -68,6 +67,7 @@ function App() {
               inputRef.current.value = data.name;
             }
             setLoading(false);
+            setError(null);
           };
           getWeatherDataWithCoordinates();
         },
@@ -83,10 +83,10 @@ function App() {
         }
       );
     } else {
-      setError(t('geolocation_error'));
+      setError(t("geolocation_error"));
       setLoading(false);
     }
-  }, [language, unit]);
+  }, []);
 
   // Fetch weather information by city name, unit, and language
   const getWeatherData = async () => {
@@ -101,7 +101,7 @@ function App() {
       setWeather(data);
       localStorage.setItem("cityName", data.name);
     } else {
-      setError("wrong_city");
+      setError(t("wrong_city"));
     }
     setLoading(false);
   };
@@ -260,83 +260,99 @@ function App() {
           </div>
         </form>
         <div className="col-span-2">
-          <div className="bg-gray-800 rounded-xl p-8 flex flex-col md:flex-row justify-between">
+          {error ? (
+            <div
+              className="p-4 mb-4 font-bold text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+              role="alert"
+              aria-live="assertive"
+            >
+              {error}
+            </div>
+          ) : (
             <div>
-              <div className="flex flex-col gap-4 lg:flex-row lg:justify-between text-white mb-2">
-                <span>
-                  {weather?.name}, {weather?.sys?.country}
-                </span>
-                <p>
-                  {new Date((weather?.dt ?? 0) * 1000).toLocaleTimeString(
-                    "en-US",
-                    {
-                      day: "numeric",
-                      month: "long",
-                      year: "numeric",
-                      hour: "2-digit",
-                      minute: "2-digit",
-                      second: "2-digit",
-                      hour12: false,
-                    }
-                  )}
-                </p>
-              </div>
-              <div className="flex flex-col gap-4 text-white">
-                <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-3">
-                  <p className="text-6xl">
-                    {weather?.main?.temp.toFixed(0)}°
-                    {weather?.unit === "metric" ? "C" : "F"}
-                  </p>
-                  <div className="grid gap-1">
-                    <p>
-                      Min / Max:
-                      <span className="mx-1">
-                        {weather?.main.temp_min.toFixed(0)}
-                      </span>
-                      \
-                      <span className="mx-1">
-                        {weather?.main?.temp_max.toFixed(0)}
-                      </span>
-                      °{weather?.unit === "metric" ? "C" : "F"}
-                    </p>
-                    <p>
-                      {t("feels_like")}:
+              {loading ? (
+                t("loading")
+              ) : (
+                <div className="bg-gray-800 rounded-xl p-8 flex flex-col md:flex-row justify-between">
+                  <div>
+                    <div className="flex flex-col gap-4 lg:flex-row lg:justify-between text-white mb-2">
                       <span>
-                        {weather?.main?.feels_like.toFixed(0) ?? ""}°
-                        {weather?.unit === "metric" ? "C" : "F"}
+                        {weather?.name}, {weather?.sys?.country}
                       </span>
-                    </p>
+                      <p>
+                        {new Date((weather?.dt ?? 0) * 1000).toLocaleTimeString(
+                          "en-US",
+                          {
+                            day: "numeric",
+                            month: "long",
+                            year: "numeric",
+                            hour: "2-digit",
+                            minute: "2-digit",
+                            second: "2-digit",
+                            hour12: false,
+                          }
+                        )}
+                      </p>
+                    </div>
+                    <div className="flex flex-col gap-4 text-white">
+                      <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-3">
+                        <p className="text-6xl">
+                          {weather?.main?.temp.toFixed(0)}°
+                          {weather?.unit === "metric" ? "C" : "F"}
+                        </p>
+                        <div className="grid gap-1">
+                          <p>
+                            Min / Max:
+                            <span className="mx-1">
+                              {weather?.main.temp_min.toFixed(0)}
+                            </span>
+                            \
+                            <span className="mx-1">
+                              {weather?.main?.temp_max.toFixed(0)}
+                            </span>
+                            °{weather?.unit === "metric" ? "C" : "F"}
+                          </p>
+                          <p>
+                            {t("feels_like")}:
+                            <span>
+                              {weather?.main?.feels_like.toFixed(0) ?? ""}°
+                              {weather?.unit === "metric" ? "C" : "F"}
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+                      <div className="grid grid-cols-2 md:grid-cols-4 gap-y-4 md:gap-x-4 lg:gap-x-4">
+                        {weatherInformation.map((item, index) => (
+                          <div
+                            className={`${
+                              !item.value ? "hidden" : "flex"
+                            } items-center`}
+                            key={index}
+                          >
+                            {item.label}:{item.value}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex justify-between">
+                    {weather?.weather.map((item, index) => (
+                      <div key={index} className="text-center">
+                        <img
+                          className="w-32"
+                          src={`https://openweathermap.org/img/wn/${item.icon}@4x.png`}
+                          alt={item.description}
+                          role="img"
+                          aria-label={item.description}
+                        />
+                        <p className="text-white">{item.description}</p>
+                      </div>
+                    ))}
                   </div>
                 </div>
-                <div className="grid grid-cols-2 md:grid-cols-4 gap-y-4 md:gap-x-4 lg:gap-x-4">
-                  {weatherInformation.map((item, index) => (
-                    <div
-                      className={`${
-                        !item.value ? "hidden" : "flex"
-                      } items-center`}
-                      key={index}
-                    >
-                      {item.label}:{item.value}
-                    </div>
-                  ))}
-                </div>
-              </div>
+              )}
             </div>
-            <div className="flex justify-between">
-              {weather?.weather.map((item, index) => (
-                <div key={index} className="text-center">
-                  <img
-                    className="w-32"
-                    src={`https://openweathermap.org/img/wn/${item.icon}@4x.png`}
-                    alt={item.description}
-                    role="img"
-                    aria-label={item.description}
-                  />
-                  <p className="text-white">{item.description}</p>
-                </div>
-              ))}
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
